9f0cb8de7455b98c75ba4d2f0af117e1293e365a
Tom Overland <tomoverland03@gmail.com>
2020-08-05 18:08:31 -0500

    made changes to script

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+const notificationElement = document.querySelector(".notification");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+});
